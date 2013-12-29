29bb823e577f7cc0628bdbe798d57e6dbd1abeae
Eugene Janusov <esycat@gmail.com>
2013-12-30 04:00:31 +1100

    The configuration property is now named `namespace`.

--- src/main/groovy/com/esyfur/gradle/util/ConfigExpander.groovy
+++ src/main/groovy/com/esyfur/gradle/util/ConfigExpander.groovy
@@ -8,16 +8,14 @@ import org.gradle.api.GradleException
 private class ConfigExpander {
 
     private final Project project
-    private final String propName
+    private final String namespace
 
     def ConfigExpander(final Project project) {
         this.project = project
 
         // Set config property name: either provided by the user or default
-        this.propName = project.ext.has(ConfigPlugin.NAME + '.propName') ?
-                        project.ext.get(ConfigPlugin.NAME + '.propName').toString().trim() : ConfigPlugin.NAME
-
-        project.logger.info(sprintf('Config property name: %s', propName))
+        this.namespace = getNamespace()
+        project.logger.info(sprintf('Config property name: %s', namespace))
 
         // Initializing config object
         checkConfig()
@@ -74,17 +72,17 @@ private class ConfigExpander {
     }
 
     public ConfigObject getConfig() {
-        project.ext.get(propName)
+        project.ext.get(namespace)
     }
 
     private ConfigExpander setConfig(ConfigObject config) {
-        project.ext.set(propName, config)
+        project.ext.set(namespace, config)
         this
     }
 
     private void checkConfig() {
-        if (project.ext.has(propName)) {
-            def config = project.ext.get(propName)
+        if (project.ext.has(namespace)) {
+            def config = project.ext.get(namespace)
 
             if (config instanceof ConfigObject) {
                 def errMsg = 'Config object has been already initialized for this project.'
@@ -92,9 +90,14 @@ private class ConfigExpander {
             }
             else {
                 def errMsg = 'The given project already has property %s of type %s, but %s expected.'
-                throw new GradleException(sprintf(errMsg, propName, config.getClass(), ConfigObject.getSimpleName()))
+                throw new GradleException(sprintf(errMsg, namespace, config.getClass(), ConfigObject.getSimpleName()))
             }
         }
+    }
+
+    private String getNamespace() {
+        def propName = ConfigPlugin.NAME + '.namespace'
+        project.ext.has(propName) ? project.ext.get(propName).toString().trim() : ConfigPlugin.NAME
     }
 
 }
