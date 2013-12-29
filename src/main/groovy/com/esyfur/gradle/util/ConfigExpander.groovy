19c9dc51cb1bbd5be7ea41b0384e289d04256dd5
Eugene Janusov <esycat@gmail.com>
2013-12-30 04:30:29 +1100

    Simplified ConfigExpander.

--- src/main/groovy/com/esyfur/gradle/util/ConfigExpander.groovy
+++ src/main/groovy/com/esyfur/gradle/util/ConfigExpander.groovy
@@ -7,19 +7,12 @@ import org.gradle.api.GradleException
 
 private class ConfigExpander {
 
+    private final ConfigObject config = new ConfigObject()
     private final Project project
-    private final String namespace
 
     def ConfigExpander(final Project project) {
         this.project = project
-
-        // Set config property name: either provided by the user or default
-        this.namespace = getNamespace()
-        project.logger.info(sprintf('Config property name: %s', namespace))
-
-        // Initializing config object
-        checkConfig()
-        setConfig(new ConfigObject())
+        initConfig()
     }
 
     /**
@@ -44,25 +37,22 @@ private class ConfigExpander {
 
     void apply(Properties properties) {
         def slurper = new ConfigSlurper()
-        slurper.setBinding(getConfig().toProperties())
+        slurper.setBinding(this.config.toProperties())
         def config = slurper.parse(properties)
 
         apply(config)
     }
 
     void apply(ConfigObject config) {
-        def localConfig = config.clone()
-
         try {
-            localConfig.merge(getConfig())
+            def properties = config.clone().merge(this.config)
+            this.config.putAll(properties)
         }
         catch (ex) {
             project.logger.error('Unable to merge config values.')
             project.logger.debug('Config object: ' + localConfig)
             throw ex
         }
-
-        setConfig(localConfig)
     }
 
     public static ConfigObject slurp(Path configFile, Map<String, Object> bindings) {
@@ -71,16 +61,15 @@ private class ConfigExpander {
         slurper.parse(configFile.toUri().toURL())
     }
 
-    public ConfigObject getConfig() {
-        project.ext.get(namespace)
+    private String getNamespace() {
+        def propName = ConfigPlugin.NAME + '.namespace'
+        project.ext.has(propName) ? project.ext.get(propName).toString().trim() : ConfigPlugin.NAME
     }
 
-    private ConfigExpander setConfig(ConfigObject config) {
-        project.ext.set(namespace, config)
-        this
-    }
+    private void initConfig() {
+        def namespace = getNamespace()
+        project.logger.info(sprintf('Config property name: %s', namespace))
 
-    private void checkConfig() {
         if (project.ext.has(namespace)) {
             def config = project.ext.get(namespace)
 
@@ -93,11 +82,8 @@ private class ConfigExpander {
                 throw new GradleException(sprintf(errMsg, namespace, config.getClass(), ConfigObject.getSimpleName()))
             }
         }
-    }
 
-    private String getNamespace() {
-        def propName = ConfigPlugin.NAME + '.namespace'
-        project.ext.has(propName) ? project.ext.get(propName).toString().trim() : ConfigPlugin.NAME
+        project.ext.set(namespace, config)
     }
 
 }
