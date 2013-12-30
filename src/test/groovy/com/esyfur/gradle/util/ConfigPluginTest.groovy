488549e51f6180141aaa0525e5e26bb0b603c1e9
Eugene Janusov <esycat@gmail.com>
2013-12-30 15:04:29 +1100

    Some ad hoc testing.

---
+++ src/test/groovy/com/esyfur/gradle/util/ConfigPluginTest.groovy
@@ -0,0 +1,15 @@
+package com.esyfur.gradle.util
+
+import org.gradle.api.Project
+
+public class ConfigPluginTest {
+
+    private Map props = ['a.b.c': 'Hi', 'a.b.d': 'Yo', 'f.g.h': 'Howdy']
+
+    public void pluginCreatesConfigObjectOnProject() {
+    }
+
+    public void loadDoesNotOverrideExistingValues() {
+    }
+
+}
--- src/test/groovy/com/esyfur/gradle/util/Plugin.groovy
+++
@@ -1,9 +0,0 @@
-package com.esyfur.gradle.util
-
-import org.gradle.api.Project
-
-public class ConfigTest {
-
-    private Map props = ['a.b.c': 'Hi', 'a.b.d': 'Yo', 'f.g.h': 'Howdy']
-
-}
